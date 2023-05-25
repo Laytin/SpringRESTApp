eadc98e8537d00569a7831bbaf405b994c84f695
Laytin <xpor2002@gmail.com>
2023-05-26 00:43:05 +0300

    added gettrips

--- src/main/java/com/laytin/SpringRESTApp/controllers/TripController.java
+++ src/main/java/com/laytin/SpringRESTApp/controllers/TripController.java
@@ -1,22 +1,28 @@
 package com.laytin.SpringRESTApp.controllers;
 
 import com.laytin.SpringRESTApp.dto.TripDTO;
+import com.laytin.SpringRESTApp.models.City;
 import com.laytin.SpringRESTApp.models.Trip;
 import com.laytin.SpringRESTApp.security.CustomerDetails;
 import com.laytin.SpringRESTApp.services.TripService;
 import com.laytin.SpringRESTApp.utils.TripValidator;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/trip")
@@ -38,5 +44,25 @@ public class TripController {
             throw new RuntimeException();
         tripService.register(t,(CustomerDetails)auth.getPrincipal());
         return ResponseEntity.ok(HttpStatus.OK);
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<HttpStatus> edit(@PathVariable("id") int id,@RequestBody @Valid TripDTO tripDTO, BindingResult result, Authentication auth){
+        Trip t = modelMapper.map(tripDTO, Trip.class);
+        tripValidator.validate(t,result);
+        if(result.hasErrors())
+            throw new RuntimeException();
+        if(!tripService.edit(id,t,(CustomerDetails)auth.getPrincipal()))
+            throw new RuntimeException();
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+    @GetMapping()
+    public List<Trip> getTrips(@RequestParam(value = "date",  required = true) @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS") LocalDate timing,
+                               @RequestParam(value = "place_from", required = true) City place_from,
+                               @RequestParam(value = "place_to", required = true) City place_to,
+                               @RequestParam(value = "sits", required = true) int sits){
+        List<Trip> trips = tripService.getTrips(timing,place_from,place_to,sits);
+        if(trips==null || trips.isEmpty())
+            return new ArrayList<>();
+        return trips;
     }
 }
--- src/main/java/com/laytin/SpringRESTApp/services/TripService.java
+++ src/main/java/com/laytin/SpringRESTApp/services/TripService.java
@@ -1,27 +1,46 @@
 package com.laytin.SpringRESTApp.services;
 
-import com.laytin.SpringRESTApp.models.Car;
+import com.laytin.SpringRESTApp.dao.TripDAO;
+import com.laytin.SpringRESTApp.models.City;
 import com.laytin.SpringRESTApp.models.Trip;
 import com.laytin.SpringRESTApp.repositories.CarRepository;
 import com.laytin.SpringRESTApp.repositories.TripRepository;
 import com.laytin.SpringRESTApp.security.CustomerDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.BindingResult;
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TripService {
     private final TripRepository tripRepository;
     private final CarRepository carRepository;
+    private final TripDAO tripDAO;
     @Autowired
-    public TripService(TripRepository tripRepository, CarRepository carRepository) {
+    public TripService(TripRepository tripRepository, CarRepository carRepository, TripDAO tripDAO) {
         this.tripRepository = tripRepository;
         this.carRepository = carRepository;
+        this.tripDAO = tripDAO;
     }
     public void register(Trip t, CustomerDetails cd){
         t.setCustomer(cd.getCustomer());
         tripRepository.save(t);
+    }
+
+    public boolean edit(int id,Trip t, CustomerDetails principal) {
+        Optional<Trip> edited = tripRepository.findById(id);
+        if(edited.isEmpty() || edited.get().getCustomer().getId()!= principal.getCustomer().getId())
+            return false;
+        t.setCustomer(edited.get().getCustomer());
+        t.setId(id);
+        tripRepository.save(t);
+        return true;
+    }
+
+    public List<Trip> getTrips(LocalDate tm, City placeFrom, City placeTo, int sits) {
+        return tripDAO.getTrips(tm,placeFrom,placeTo,sits);
     }
 }
