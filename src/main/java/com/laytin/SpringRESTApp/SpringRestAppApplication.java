69f8beab7d8e5ae81e38f68f0f90f044ecf84cb1
Laytin <xpor2002@gmail.com>
2023-06-03 15:26:07 +0300

    Added configuration to get rid of a few DTOs and store some fields in DTO

--- src/main/java/com/laytin/SpringRESTApp/SpringRestAppApplication.java
+++ src/main/java/com/laytin/SpringRESTApp/SpringRestAppApplication.java
@@ -1,9 +1,14 @@
 package com.laytin.SpringRESTApp;
 
+import com.laytin.SpringRESTApp.dto.CarDTO;
 import com.laytin.SpringRESTApp.dto.CustomerDTO;
+import com.laytin.SpringRESTApp.dto.TripDTO;
+import com.laytin.SpringRESTApp.models.Car;
 import com.laytin.SpringRESTApp.models.Customer;
+import com.laytin.SpringRESTApp.models.Trip;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
@@ -24,6 +29,19 @@ public class SpringRestAppApplication {
 				skip(destination.getId());
 			}
 		});
+		modelMapper.addMappings(new PropertyMap<CarDTO, Car>() {
+			@Override
+			protected void configure() {
+				skip(destination.getId());
+			}
+		});
+		modelMapper.addMappings(new PropertyMap<TripDTO, Trip>() {
+			@Override
+			protected void configure() {
+				skip(destination.getId());
+			}
+		});
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
 		return modelMapper;
 	}
 }
